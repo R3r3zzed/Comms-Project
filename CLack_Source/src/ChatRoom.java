246a3a20af0f22c573d72df3b746957b0ee4f9d5
R3r3zzed <67608988+R3r3zzed@users.noreply.github.com>
2023-11-19 11:10:15 -0800

    Add files via upload
    Initial upload. Some things are new and could be useful. Let me know what to edit.

--- CLack_Source/src/ChatRoom.java
+++ CLack_Source/src/ChatRoom.java
@@ -1,5 +1,72 @@
+package Implementation;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import java.util.stream.Collectors;
 
 public class ChatRoom {
+	private String chatID;
+	private List<Message> history;
+	private List<ServerHandler> participantHandlers;
 
+	
+	
+	public ChatRoom(String chatID){
+		this.chatID = chatID;
+		this.history = new ArrayList<>();
+		this.participantHandlers = new ArrayList<>();
+
+	}
+	
+	
+	//getters
+	
+	public String getChatID() {
+		return chatID;
+	}
+	
+	public List<String> getParticipantIDs() {
+        // Assuming ServerHandler has a method to get the participant's ID (e.g., getClientId())
+        return participantHandlers.stream()
+                                  .map(handler -> handler.getClientId()) // This method needs to exist in ServerHandler
+                                  .distinct() // To ensure unique IDs
+                                  .collect(Collectors.toList());
+    }
+	
+	
+	//Changing chatroom participants
+	
+	   public void addParticipantHandler(ServerHandler handler) {
+	        if (!participantHandlers.contains(handler)) {
+	            participantHandlers.add(handler);
+	            System.out.println("User " + handler.getParticipantID() + " has been added.");
+	        }
+	    }
+	
+	   public void removeParticipantHandler(ServerHandler handler) {
+	        participantHandlers.remove(handler);
+	        System.out.println("User " + handler.getParticipantID() + " has been removed.");
+	    }
+	
+
+	   
+	   
+	//Message methods
+	
+	public void sendMessage(Message message) {
+		history.add(message);
+		
+		for(ServerHandler handler: participantHandlers) {
+			handler.SendNewMsg(message);
+		}
+	}
+	
+	public void displayMessage(Message message) {
+		System.out.println(message.getContent());
+	}
+	
+	public List<Message> getHistory(){
+		return new ArrayList<>(history);
+	}
 }
