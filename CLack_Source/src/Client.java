48dffa7768d0074f53c98735c70e09cb5f5eea7f
seanstev0710 <seanstev0710@gmail.com>
2023-11-25 16:54:08 -0800

    Added main for Client
    Has small snippet of code that opens Login Interface

--- CLack_Source/src/Client.java
+++ CLack_Source/src/Client.java
@@ -1,4 +1,18 @@
 
 public class Client {
-
+	public static void main(String args[]) {
+		
+		/*
+		 * Opens the login screen. For testing
+		 */
+		Client client = new Client();
+		
+		LoginUI loginScreen = new LoginUI();
+		loginScreen.display(client);
+		
+		while(loginScreen.isLoggedIn() == false) {
+			// don't open anything else while not logged in
+		}
+		
+	}
 }
