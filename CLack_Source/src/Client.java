5723666d21247e18864629a29ce3b76b70d051a3
seanstev0710 <seanstev0710@gmail.com>
2023-11-25 17:18:43 -0800

    Added login method for Client
    login method is not fully implemented, it is currently a test

--- CLack_Source/src/Client.java
+++ CLack_Source/src/Client.java
@@ -2,7 +2,7 @@
 public class Client {
 	public static void main(String args[]) {
 		
-		/*
+		/* 
 		 * Opens the login screen. For testing
 		 */
 		Client client = new Client();
@@ -14,5 +14,31 @@ public class Client {
 			// don't open anything else while not logged in
 		}
 		
+		//
+		// Setup client information; Server <-> Client interactions
+		//
+		
+		MainUI mainUI = new MainUI();
+		mainUI.display(client);
+		loginScreen.close();
+		/*
+		 * For testing
+		 */
+		
+	}
+	
+	public boolean login(String username, String password) {
+		/*
+		 * For testing
+		 */
+		String name = "admin";
+		String pass = "admin";
+		if(name.compareTo(username) != 0 || pass.compareTo(password) != 0) {
+			return false;
+		}
+		return true;
+		/*
+		 * For testing
+		 */
 	}
 }
