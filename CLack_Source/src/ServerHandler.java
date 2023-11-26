cfc9dcc4d0f8c85ba29006a657263d537f70be09
MaddioAngel <maddiotomboy@yahoo.com>
2023-11-25 21:52:16 -0800

    adding first time method and the main

--- CLack_Source/src/ServerHandler.java
+++ CLack_Source/src/ServerHandler.java
@@ -5,30 +5,15 @@ class ServerHandler implements Runnable {
 		private final Socket clientSocket;
 		private String ip;
 		private ChatRoom[] currentChatRooms;
-		
 		private ObjectInputStream objectInputStream;
 		private ObjectOutputStream objectOutputStream;
+		private User currentUser;
 		
 		private static Server ServerInfo = new Server(); //each thread made has access to the same server object
 		
 		public ServerHandler(Socket socket){
 			this.clientSocket = socket;
 			this.ip = this.clientSocket.getInetAddress().getHostAddress();
-			
-				
-				//create the in and out to the client 
-				try {
-					OutputStream outputStream = this.clientSocket.getOutputStream();
-					InputStream inputStream = this.clientSocket.getInputStream();
-					this.objectOutputStream = new ObjectOutputStream(outputStream);
-					this.objectInputStream = new ObjectInputStream(inputStream);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				
-		
-			
-			
 		}		
 		
 		public String getIp() {
@@ -44,14 +29,57 @@ class ServerHandler implements Runnable {
 		}
 	 
 		public void FirstTimeLogin() {
+			//Sent all messages and chats with this user in it
+			//Step 1: Find the chats with user in it
+			ChatRoom[] userChatroomsFromServer = ServerInfo.getUserChatrooms(this.currentUser.getUsername());
 			
+			//Step 2: Send them over the objectOutputStream
+			for (int i = 0; i < userChatroomsFromServer.length; i++) {  
+				Message[] msgs = userChatroomsFromServer[i].getHistory();
+				for (int j = 0; j < msgs.length; j++){
+					try {
+						objectOutputStream.writeObject(msgs[j]);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}	
+			}
+			
+			//Step 3: Send empty object to signal end of transfer
+			try {
+				objectOutputStream.writeObject(new Message(null, null, null, null, null));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			//Step 4: Update Server to show user is online
+			ServerInfo.updateUserStatus(this.currentUser.getUsername());
+				
+		}
+		public Message ReceiveNewMsg() {
+			// look through the users chat rooms for a new message and send to the client
+			
+			return null;
 			
 		}
-		public void ReceiveNewMsg() {
-			
+		public void sendNewMessage() {
+			//Receives a new message from Client from the objectInputStream
+			try {
+				Message newMessage = (Message) objectInputStream.readObject();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//updates the server so all the threads of other clients' get the message
+			updateServerWithNewMessage();
+						
 		}
-		public void SendNewMsg() {
-			
+		
+		public void updateServerWithNewMessage() {
+
 		}
 		
 		public void logOut() {
@@ -60,7 +88,48 @@ class ServerHandler implements Runnable {
 
 		public void run(){
 			//port = 1234
-			
+			//create the in and out to the client 
+			try {
+				OutputStream outputStream = this.clientSocket.getOutputStream();
+				InputStream inputStream = this.clientSocket.getInputStream();
+				this.objectOutputStream = new ObjectOutputStream(outputStream);
+				this.objectInputStream = new ObjectInputStream(inputStream);
+
+
+				//Who is logging on
+				boolean doesntUser = true;
+				while(doesntUser){
+					String UserAndPassword;
+					try {
+						UserAndPassword = (String) objectInputStream.readObject();
+						String[] userInfo = UserAndPassword.split(";;;");
+						doesntUser = ! ServerInfo.UserAuthentication(userInfo[0], userInfo[1]);
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+
+				 FirstTimeLogin();
+
+				//get the different messages from client
+				while (true) {
+					// Both method for getting new messages and sending new messages get its own thread
+					//Getting messages from server is constant but getting messages from client is main thread (need to know when to log out)
+					
+					//get new messages from server
+					
+		
+					//get new messages from client
+					
+					break;
+				}
+				
+				logOut();
+				
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
 		}
 			
 	}
