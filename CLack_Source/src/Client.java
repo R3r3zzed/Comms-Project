735c8c168d0d1a5c8edc319c6de179a02e7be285
Joseph Fujimoto <83256590+jsfmu@users.noreply.github.com>
2023-11-27 10:17:22 -0800

    Update Client.java
    GUI pops up when ran

--- CLack_Source/src/Client.java
+++ CLack_Source/src/Client.java
@@ -2,78 +2,113 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Client {
-	public static void main(String args[]) {
-		/* 
-		 * For testing
-		 * Opens the login screen.
-		 */
-		Client client = new Client();
-		
-		LoginUI loginScreen = new LoginUI();
-		loginScreen.display(client);
-		
-		while(loginScreen.isLoggedIn() == false) {
-			// don't open anything else while not logged in
-			System.out.println(loginScreen.isLoggedIn());
-		}
-		
-		//
-		// Setup client information; Server <-> Client interactions
-		//
-	
-		MainUI mainUI = new MainUI();
-		mainUI.display(client);
-		
-		/*
-		 * For testing
-		 */
-		
-	}
-	
-	private static Socket createSocket(String ipad, int portnum) throws IOException {
-		return new Socket(ipad, portnum);
-	}
-	
-	private static String getUserInput(Scanner sc) {
-		System.out.println("\nEnter: ");
-		return sc.nextLine();
-	}
-	
-	public boolean login(String username, String password) throws IOException, ClassNotFoundException{
-		/*
-		 * For testing
-		 */
-		String name = "admin";
-		String pass = "admin";
-		if(!(name.equals(username)) || !(pass.equals(password)))
-			return false;
-		return true;
-		/*
-		 * For testing
-		 */
-	}
-	
-	public void requestLogs(String username) {
-		
-	}
-	
-	public int findUserFromDirectory(User u) {
-		return 0;
-		
-	}
-	
-	public void openChatRoom(String id) {
-		
-	}
-	
-	public void loadChatRoom(String id) {
-		
-	}
-	
-	public void sendMessageToServer() {
-		
-	}
+
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private String serverIP;
+    private int serverPort;
+
+    public Client(String serverIP, int serverPort) {
+        this.serverIP = serverIP;
+        this.serverPort = serverPort;
+    }
+
+    // Establishes connection with the server
+    public boolean connect() {
+        try {
+            this.socket = new Socket(this.serverIP, this.serverPort);
+            this.out = new ObjectOutputStream(socket.getOutputStream());
+            this.in = new ObjectInputStream(socket.getInputStream());
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Attempts to log in to the server
+    public boolean login(String username, String password) {
+        try {
+            this.out.writeObject(username + ": " + password);
+            this.out.flush();
+
+            // Await response from the server
+            String response = (String) this.in.readObject();
+            return Boolean.parseBoolean(response);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Requests chat logs from the server
+    public void requestLogs(String username) {
+        try {
+            this.out.writeObject("LOG_REQUEST: " + username);
+            this.out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Opens a chat room
+    public void openChatRoom(String id) {
+        try {
+            this.out.writeObject("OPEN_CHAT: " + id);
+            this.out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Loads messages from a chat room
+    public void loadChatRoom(String id) {
+        try {
+            this.out.writeObject("LOAD_CHAT: " + id);
+            this.out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Sends a message to the server
+    public void sendMessageToServer(String message) {
+        try {
+            this.out.writeObject("MESSAGE: " + message);
+            this.out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Close the connection
+    public void close() {
+        try {
+            if (socket != null) 
+                socket.close();
+            if (in != null) 
+                in.close();
+            if (out != null)
+                out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client("127.0.0.1", 1234); // Example IP and port
+
+        LoginUI loginScreen = new LoginUI();
+        loginScreen.display(client);
+
+        while (!loginScreen.isLoggedIn()) {
+            // Wait for successful login
+        }
+
+        MainUI mainUI = new MainUI();
+        mainUI.display(client);
+    }
 }
