00b7b13fb3d53162caac434c9c95368222bf4c10
seanstev0710 <seanstev0710@gmail.com>
2023-11-27 22:11:51 -0800

    Added placeComponents function to MainUI
    added how components will be placed on MainUI. No functionality yet.

--- CLack_Source/src/MainUI.java
+++ CLack_Source/src/MainUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
@@ -10,30 +12,32 @@ public class MainUI implements GUI{
 	private Client client;
 	private JFrame frame;
 	private JPanel panel;
-	private JScrollPane directoryScrollPane;
+	private User currentUser;
 	
 	private JTextField filterDirectoryTextField;
 	private JButton filterDirectorySubmitButton;
-	private Vector<JButton> selectUserButtons;
+	private JScrollPane directoryScrollPane;
+	private Vector<JLabel> directoryLabels;
 	private Vector<User> directory;
-	private JScrollPane chatScrollPane;
 	
 	private JTextField filterChatRoomTextField;
 	private JButton filterChatRoomSubmitButton;
 	private Vector<JButton> chatRoomButtons;
 	private Vector<ChatRoom> chatRooms;
+	private JScrollPane chatScrollPane;
+	
+	private JButton createChatRoomButton;
 	private JButton logoutButton;
-	
 	private JButton viewLogsButton;
-	
-	private User currentUser;
 	private JLabel userLabel;
+	
 	private ChatUI chatUI;
 	private LogsUI logsUI;
 	
 	@Override
 	public void display(Client client) {
 		this.client = client;
+		// currentUser = client.getCurrentUser;		// needs to be implemented in Client class
 		frame = new JFrame("CLack");
 		panel = new JPanel();
 		createUI();
@@ -41,17 +45,59 @@ public class MainUI implements GUI{
 	}
 	
 	private void createUI() {
-		frame.setVisible(true);
+		placeComponents();
 		frame.setSize(1280, 720);
+		frame.setVisible(true);
 	}
 	
 	// tells the client to close. Closes sockets
 	private void logout() {
 		client.close();
+		frame.setVisible(false);
+		frame.dispose();
+	}
+	
+	// handles setting up how buttons will placed on panel
+	private void placeComponents() {
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridy = 0;
+		constraints.gridx = 0;
+		panel.add(userLabel,constraints);
+		
+		// display logs view button only if user is IT user
+		if(currentUser.getUserType() == UserType.IT) {
+			constraints.gridx = 1;
+			constraints.gridy = 0;
+			panel.add(viewLogsButton, constraints);
+		}
+		
+		constraints.gridx = 2;
+		constraints.gridy = 0;
+		constraints.gridwidth = 1;
+		panel.add(createChatRoomButton, constraints);
+		
+		constraints.gridx = 3;
+		constraints.gridy = 0;
+		constraints.gridwidth = 1;
+		panel.add(logoutButton, constraints);
+		
+		constraints.fill = constraints.VERTICAL;
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		constraints.gridwidth = 2;
+		panel.add(directoryScrollPane, constraints);
+		
+		constraints.fill = constraints.VERTICAL;
+		constraints.gridx = 2;
+		constraints.gridy = 1;
+		constraints.gridwidth = 2;
+		panel.add(chatScrollPane, constraints);
 	}
 	
 	private void doFilterDirectory(String filter) {
-		
+		if (filter.compareTo(filter) == 0 || filter == null) {
+			
+		}
 	}
 	
 	private void doFilterChatRooms(String filter) {
