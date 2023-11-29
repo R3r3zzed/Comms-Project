0f677dd7eab6039a067091f53d1e039992ff6435
Joseph Fujimoto <83256590+jsfmu@users.noreply.github.com>
2023-11-28 22:03:41 -0800

    Update Client.java
    Added currentUser, directory and rooms and their public methods, simplified code

--- CLack_Source/src/Client.java
+++ CLack_Source/src/Client.java
@@ -2,26 +2,34 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Vector;
 
 public class Client {
-
     private Socket socket;
-    private ObjectInputStream in;
-    private ObjectOutputStream out;
+    private ObjectInputStream input;
+    private ObjectOutputStream output;
     private String serverIP;
     private int serverPort;
+    
+    // -
+    private User currentUser;
+    private Vector<User> directory;
+    private Vector<ChatRoom> rooms;
 
     public Client(String serverIP, int serverPort) {
         this.serverIP = serverIP;
         this.serverPort = serverPort;
+        this.directory = new Vector<>();
+        this.rooms = new Vector<>();
     }
-
+    
+    // +
     // Establishes connection with the server
     public boolean connect() {
         try {
             this.socket = new Socket(this.serverIP, this.serverPort);
-            this.out = new ObjectOutputStream(socket.getOutputStream());
-            this.in = new ObjectInputStream(socket.getInputStream());
+            this.output = new ObjectOutputStream(socket.getOutputStream());
+            this.input = new ObjectInputStream(socket.getInputStream());
             return true;
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,12 +40,11 @@ public class Client {
     // Attempts to log in to the server
     public boolean login(String username, String password) {
         try {
-            this.out.writeObject(username + ": " + password);
-            this.out.flush();
-
-            // Await response from the server
-            String response = (String) this.in.readObject();
-            return Boolean.parseBoolean(response);
+            this.output.writeObject(username + ";;;" + password);
+            this.output.flush();
+            if (this.input.readObject() instanceof User)
+                this.currentUser = (User) this.input.readObject();                
+            return this.input.readObject() instanceof User;
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
             return false;
@@ -47,18 +54,18 @@ public class Client {
     // Requests chat logs from the server
     public void requestLogs(String username) {
         try {
-            this.out.writeObject("LOG_REQUEST: " + username);
-            this.out.flush();
+            this.output.writeObject("LOG_REQUEST: " + username);
+            this.output.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     // Opens a chat room
-    public void openChatRoom(String id) {
+    public void openChatRoom(String id){
         try {
-            this.out.writeObject("OPEN_CHAT: " + id);
-            this.out.flush();
+            this.output.writeObject("OPEN_CHAT: " + id);
+            this.output.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -67,8 +74,8 @@ public class Client {
     // Loads messages from a chat room
     public void loadChatRoom(String id) {
         try {
-            this.out.writeObject("LOAD_CHAT: " + id);
-            this.out.flush();
+            this.output.writeObject("LOAD_CHAT: " + id);
+            this.output.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -77,38 +84,56 @@ public class Client {
     // Sends a message to the server
     public void sendMessageToServer(String message) {
         try {
-            this.out.writeObject("MESSAGE: " + message);
-            this.out.flush();
+            this.output.writeObject("MESSAGE: " + message);
+            this.output.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    public void updateDirectory(Vector<User> updatedDirectory) {
+        this.directory = updatedDirectory;
+    }
+    
+    public void addRoom(ChatRoom room) {
+    	if(room == null)
+        this.rooms.add(room);
+    }
+    
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public Vector<User> getDirectory() {
+        return directory;
+    }
+
+    public Vector<ChatRoom> getRooms() {
+        return rooms;
     }
 
     // Close the connection
     public void close() {
         try {
-            if (socket != null) 
+            if (socket != null || input != null || output != null)
                 socket.close();
-            if (in != null) 
-                in.close();
-            if (out != null)
-                out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        Client client = new Client("127.0.0.1", 1234); // Example IP and port
+        Client client = new Client("127.0.0.1", 1234);
 
         LoginUI loginScreen = new LoginUI();
         loginScreen.display(client);
 
-        while (!loginScreen.isLoggedIn()) {
-            // Wait for successful login
-        }
-
-        MainUI mainUI = new MainUI();
-        mainUI.display(client);
+        while (!loginScreen.isLoggedIn()){/* Wait for successful login */}
+        
+        do {
+        	MainUI mainUI = new MainUI();
+        	mainUI.display(client);
+        }while(loginScreen.isLoggedIn());
+        
     }
 }
