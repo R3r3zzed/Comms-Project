e115bc3b7d2b8c34eea0198cf8839be5c8dadcc6
dangel3 <dangel3@horizon.csueastbay.edu>
2023-12-01 15:35:31 -0800

    JUnit ClassPath Added and UserTests Added
    Added a class path for JUnit 5 and implemented a class for user tests. May need further addition of cases.

---
+++ CLack_Source/src/UserTests.java
@@ -0,0 +1,75 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class UserTests {
+	
+	 @Test
+	 public void testGenerateID() {
+		 String id1 = User.generateID();
+	     String id2 = User.generateID();
+	     assertNotEquals(id1, id2);
+	 }
+	 
+	 
+	 @Test
+	 public void testConstructorWithGeneratedID() {
+		 // Test constructor generates a unique ID 
+		 User user = new User("John Doe", "john.doe", "password123", UserType.BASIC);
+		 assertNotNull(user.getUserID());
+	 }
+	 
+	 
+	 @Test
+	 public void testConstructorWithSpecificID() {
+		 // Test constructor accepts a specific ID
+		 String specificID = "123456";
+		 User user = new User(specificID, "Jane Smith", "jane.smith", "pass456", UserType.IT);
+		 assertEquals(specificID, user.getUserID());
+	 }
+
+    @Test
+    void testGetters() {
+        // Test getter methods
+        User user = new User("John Doe", "john.doe", "password123", UserType.BASIC);
+
+        assertEquals("John Doe", user.getName());
+        assertEquals("john.doe", user.getUsername());
+        assertEquals("password123", user.getPassword());
+        assertEquals(UserStatus.OFFLINE, user.getUserStatus());
+        assertEquals(UserType.BASIC, user.getUserType());
+    }
+
+    @Test
+    void testChangeStatus() {
+        // Test changing user status
+        User user = new User("John Doe", "john.doe", "password123", UserType.BASIC);
+
+        assertEquals(UserStatus.OFFLINE, user.getUserStatus());
+
+        user.changeStatus(UserStatus.ONLINE);
+        assertEquals(UserStatus.ONLINE, user.getUserStatus());
+
+        user.changeStatus(UserStatus.OFFLINE);
+        assertEquals(UserStatus.OFFLINE, user.getUserStatus());
+    }
+    
+    @Test
+    public void testStatusChange() {
+    	// Test to make sure user status changes correctly
+        User user = new User("Jane Doe", "jane.doe", "password123", UserType.BASIC);
+
+        assertEquals(UserStatus.OFFLINE, user.getUserStatus());
+        user.changeStatus(UserStatus.ONLINE);
+        assertEquals(UserStatus.ONLINE, user.getUserStatus());
+    }
+    
+    @Test
+    public void testUniqueIDGeneration() {
+    	// Test generated IDs are unique
+        User user1 = new User("User 1", "user1", "pass1", UserType.BASIC);
+        User user2 = new User("User 2", "user2", "pass2", UserType.IT);
+
+        assertNotEquals(user1.getUserID(), user2.getUserID());
+    }
+}
