63f19984af77552c10fa825dc6abde7e9ab1d90c
seanstev0710 <seanstev0710@gmail.com>
2023-12-02 18:40:20 -0800

    LogsUI updated
    LogsUI:
    -Added functionality to allow user to select whose logs to view. Still
    needs to implement getting the logs(ChatRooms) for the selected User

--- CLack_Source/src/LogsUI.java
+++ CLack_Source/src/LogsUI.java
@@ -1,48 +1,320 @@
 import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.LayoutManager;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 
-public class LogsUI implements GUI {
-	private Client client;
+public class LogsUI implements GUI{
+	private ClientFake client;
 	private JFrame frame;
-	private JScrollPane directoryScrollPane;
-	private ArrayList<JButton> selectUserButtons;
-	private JScrollPane chatScrollPane;
-	private ArrayList<JButton> chatRoomButtons;
-	private JTextField filterDirectoryTextField;	// might need to change
+	private JPanel panel;
 	private User selectedUser;
+	
+	private JTextField filterDirectoryTextField;
+	private JButton filterDirectorySubmitButton;
+	private JScrollPane directoryScrollPane;
+	private Vector<JButton> directoryButtons;
+	
+	private Vector<ChatRoom> chatRooms;
+	private JTextField filterChatRoomTextField;
+	private JButton filterChatRoomSubmitButton;
+	private Vector<JButton> chatRoomButtons;
+	private JScrollPane chatScrollPane;
+	
 	private JLabel userLabel;
+	
 	private ChatUI chatUI;
 	
-	public LogsUI(Client client) {
+	public LogsUI(ClientFake client) {
 		this.client = client;
+		selectedUser = client.getCurrentUser();
+		chatRooms = client.getChatRooms();
 	}
 	
 	@Override
 	public void display() {
-		// TODO Auto-generated method stub
-		
-	}
+		frame = new JFrame("CLack LOGS");
+		panel = new JPanel(new GridBagLayout());
+		createUI();
 
+	}
+	
 	private void createUI() {
+		frame.setSize(1280, 720);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);	// might need to change to call client.close()
+		createPanelComponents();
+		createChatButtons();
+		createDirectoryButtons();
+		doFilterChatScrollPane(null);
+		doFilterDirectoryScrollPane(null);
+		placePanelComponents();
 		
+		// TODO needs to be implemented on client side
+		/*
+		// create new chatroom, then open chatroom UI for it
+		createChatRoomButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				client.createChatRoom()
+			}
+		});
+		*/
+		
+		// filter directory based on filter input by user
+		filterDirectorySubmitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doFilterDirectoryScrollPane(filterDirectoryTextField.getText());
+			}
+		});
+				
+		// filter chatrooms based on filter input by user
+		filterChatRoomSubmitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doFilterChatScrollPane(filterChatRoomTextField.getText());
+			}
+		});
+				
+		frame.setVisible(true);
+		frame.getContentPane().add(panel);
+		panel.updateUI();
 	}
 	
-	private void doFilterDirectory(String filter) {
+	private void createPanelComponents(){
+		userLabel = new JLabel(selectedUser.getName(), SwingConstants.CENTER);
 		
+		filterChatRoomTextField = new JTextField();
+		filterChatRoomSubmitButton = new JButton("filter ChatRooms");
+		filterDirectoryTextField = new JTextField();
+		filterDirectorySubmitButton = new JButton("filter Directory");
+		
+		directoryButtons = new Vector<JButton>();
+		directoryScrollPane = new JScrollPane();
+		chatRoomButtons = new Vector<JButton>();
+		chatScrollPane = new JScrollPane();
 	}
 	
-	private void doFilterChatRooms(String filter) {
+	// handles setting up how buttons will placed on panel
+	// does not handle functionality
+	private void placePanelComponents() {
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.weightx = 1;
+		constraints.weighty = 0.1;
+		constraints.gridy = 0;
+		constraints.gridx = 0;
+		constraints.gridwidth = 1;
+		userLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		panel.add(userLabel,constraints);
 		
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1;
+		constraints.weighty = 0.1;
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		constraints.gridwidth = 1;
+		panel.add(filterDirectoryTextField, constraints);
+		
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1;
+		constraints.weighty = 0.1;
+		constraints.gridx = 1;
+		constraints.gridy = 1;
+		constraints.gridwidth = 1;
+		panel.add(filterDirectorySubmitButton, constraints);
+		
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1;
+		constraints.weighty = 0.1;
+		constraints.gridx = 2;
+		constraints.gridy = 1;
+		constraints.gridwidth = 1;
+		panel.add(filterChatRoomTextField, constraints);
+		
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1;
+		constraints.weighty = 0.1;
+		constraints.gridx = 3;
+		constraints.gridy = 1;
+		constraints.gridwidth = 1;
+		panel.add(filterChatRoomSubmitButton, constraints);
+		
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1;
+		constraints.weighty = 1;
+		constraints.gridx = 0;
+		constraints.gridy = 2;
+		constraints.gridwidth = 2;
+		directoryScrollPane.setColumnHeaderView(new JLabel("Directory", SwingConstants.CENTER));
+		panel.add(directoryScrollPane, constraints);
+		
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1;
+		constraints.weighty = 1;
+		constraints.gridx = 2;
+		constraints.gridy = 2;
+		constraints.gridwidth = 2;
+		chatScrollPane.setColumnHeaderView(new JLabel("Chat Rooms", SwingConstants.CENTER));
+		panel.add(chatScrollPane, constraints);
 	}
 	
-	private void doUpdateDirectoryScrollPane() {
+	// create JButtons for each chatroom
+	private void createChatButtons() {
+		JPanel scrollPanel = new JPanel();
+		scrollPanel.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
 		
+		/*TODO waiting for client side implementation 
+		for(int i = 0; i < client.getChatRooms().size(); i++) {
+			ChatRoom currentChatRoom = client.getChatRooms().elementAt(i);
+			constraints.fill = GridBagConstraints.BOTH;
+			constraints.weightx = 0.5;
+			constraints.weighty = 0.5;
+			constraints.gridy = i;
+			constraints.gridx = 0;
+			
+			// Set chatroom button name
+			// name of all the users in the 
+			String participants = "";
+			for(int j = 0; j < currentChatRoom.getUsers().size(); j++) {
+				participants += currentChatRoom.getUsers().getName();
+				if(j == currentChatRoom.getUsers().size() - 1) {
+					participants += ", ";
+				}
+			}
+			chatRoomButtons.add(new JButton(participants));
+			chatRoomButtons.elementAt(i).setPreferredSize(new Dimension(directoryScrollPane.getWidth(),frame.getHeight()/10));
+			scrollPanel.add(chatRoomButtons.elementAt(i), constraints);
+		}
+		*/
+		
+		// TODO Remove for final
+		for(int i = 0; i < 10; i++) {
+			constraints.fill = GridBagConstraints.BOTH;
+			constraints.weightx = 0.5;
+			constraints.weighty = 0.5;
+			constraints.gridy = i;
+			constraints.gridx = 0;
+			chatRoomButtons.add(new JButton("Button: " + i));
+			chatRoomButtons.elementAt(i).setPreferredSize(new Dimension(directoryScrollPane.getWidth(),frame.getHeight()/10));
+			scrollPanel.add(chatRoomButtons.elementAt(i), constraints);
+		}
+		
+		// TODO implement way to open chatRoom using CHATUID
+		for(int i = 0; i < chatRoomButtons.size(); i++) {
+			chatRoomButtons.get(i).addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					// openChatRoom( UID ));
+				}
+			});
+		}
+		chatScrollPane = new JScrollPane(scrollPanel);
+	}
+		
+	// displays only the chat rooms that have the substring filter in the participant names
+	// if "" then display everything
+	private void doFilterChatScrollPane(String filter) {
+		if (filter == null) {
+			filter = "";
+		}
+		
+		/* TODO Wait for Chatroom to implemented then uncomment
+		// look through list of participants. If any of the users' name contains substring filter then make JButton visible for it
+		for(int i = 0; i < client.getChatRooms().size();i++) {
+			Vector<User> participants = client.getChatRooms().elementAt(i).getParticipants();		// TODO Needs to be implemented
+			for(int j = 0; j < participants.size(); j++) {
+				if(participants.elementAt(i).getName().contains(filter)) {
+					chatRoomButtons.elementAt(i).setVisible(true);
+				}
+				else {
+					chatRoomButtons.elementAt(i).setVisible(false);
+				}
+			}
+		}
+		*/
+		
+		// TODO remove for final
+		for(int i = 0; i < chatRoomButtons.size(); i++) {
+			if (chatRoomButtons.elementAt(i).getText().toUpperCase().contains(filter.toUpperCase())) {
+				chatRoomButtons.elementAt(i).setVisible(true);
+			}
+			else {
+				chatRoomButtons.elementAt(i).setVisible(false);
+			}
+		}
+		
+		chatScrollPane.updateUI();
 	}
 	
-	private void doOpenChatRoom() {
+	// create the Labels for each user in directory
+	// displays the name of the user;
+	private void createDirectoryButtons() {
+		JPanel scrollPanel = new JPanel();
+		scrollPanel.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
 		
+		for(int i = 0; i < client.getDirectory().size(); i++) {
+			constraints.fill = GridBagConstraints.BOTH;
+			constraints.weightx = 0.5;
+			constraints.weighty = 0.5;
+			constraints.gridy = i;
+			constraints.gridx = 0;
+			User user = client.getDirectory().elementAt(i);
+			directoryButtons.add(new JButton(user.getUserID() + ":" + user.getName()));
+			directoryButtons.elementAt(i).setPreferredSize(new Dimension(directoryScrollPane.getWidth(),frame.getHeight()/10));
+			scrollPanel.add(directoryButtons.elementAt(i), constraints);
+		}
+		
+		for(int i = 0; i < directoryButtons.size(); i++) {
+			// ask user for confirmation if they want logs for selected user
+			// if yes. Then load up the chatrooms for selected user
+			directoryButtons.elementAt(i).addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					// TODO implement actual functionality
+					String label = ((JButton) e.getSource()).getText();
+					int isConfirmed = JOptionPane.showConfirmDialog(scrollPanel, "Get logs for " + label + "?");
+					if (isConfirmed != 0) {
+						return;
+					}
+					
+					// get the user ID from the label
+					StringTokenizer strtok = new StringTokenizer(label);
+					String id = strtok.nextToken(":");
+					User newSelectedUser = client.getDirectory().get(Integer.parseInt(id));
+					if (newSelectedUser == selectedUser) {
+						return;	// user already selected
+					}
+					selectedUser = newSelectedUser;
+					userLabel.setText(selectedUser.getName());
+					panel.updateUI();
+					// TODO remove everything in this block below this
+					System.out.println("Confirmed " + ((JButton) e.getSource()).getText());
+				}
+			});
+		}
+		directoryScrollPane = new JScrollPane(scrollPanel);
+	}
+	
+	// displays only the users that have the substring filter in their names
+	// if "" then display everything
+	private void doFilterDirectoryScrollPane(String filter) {
+		if (filter == null) {
+			filter = "";
+		}
+		
+		// look through list of users. If user's name contains substring filter then create make JLabel visible for it
+		for(int i = 0; i < client.getDirectory().size();i++) {
+			if(client.getDirectory().elementAt(i).getName().toUpperCase().contains(filter.toUpperCase())) {
+				directoryButtons.elementAt(i).setVisible(true);
+			}
+			else {
+				directoryButtons.elementAt(i).setVisible(false);
+			}
+		}
+		directoryScrollPane.updateUI();
 	}
 }
