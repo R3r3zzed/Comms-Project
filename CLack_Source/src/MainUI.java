919aed7a1153b9e1f4151cbc31deba8258d1fa05
seanstev0710 <seanstev0710@gmail.com>
2023-12-03 21:35:11 -0800

    Updated MainUI, ChatUI, and LoginUI
    MainUI:
    	- added getChatUI
    	- added method to update ChatRoomScrollPanel
    LoginUI:
    	- added getMainUI
    ChatUI
    	- removed unused import

--- CLack_Source/src/MainUI.java
+++ CLack_Source/src/MainUI.java
@@ -31,6 +31,7 @@ public class MainUI implements GUI{
 	private JButton viewLogsButton;
 	private JLabel userLabel;
 	
+	private ChatUI chatUI;
 	private LogsUI logsUI;
 	
 	public MainUI(Client client) {
@@ -111,7 +112,7 @@ public class MainUI implements GUI{
 					return;
 				}
 				
-				ChatUI chatUI = new ChatUI(client, client.openChatRoom(chatID), false);
+				chatUI = new ChatUI(client, client.openChatRoom(chatID), false);
 				chatUI.display();
 			}
 		});
@@ -284,7 +285,7 @@ public class MainUI implements GUI{
 					JButton eventSource = (JButton) e.getSource();
 					StringTokenizer strtok = new StringTokenizer(eventSource.getText());
 					String id = strtok.nextToken(":");
-					ChatUI chatUI = new ChatUI(client, client.openChatRoom(id), false);
+					chatUI = new ChatUI(client, client.openChatRoom(id), false);
 					chatUI.display();
 				}
 			});
@@ -354,10 +355,19 @@ public class MainUI implements GUI{
 		directoryScrollPane.updateUI();
 	}
 	
+	public void updateChatScrollPane() {
+		createChatButtons();
+		panel.updateUI();
+	}
+	
 	// tells the client to close. Closes sockets
 	public void close() {
 		frame.setVisible(false);
 		frame.dispose();
 		client.close();
 	}
+	
+	 public ChatUI getChatUI() {
+		 return chatUI;
+	 } 
 }
