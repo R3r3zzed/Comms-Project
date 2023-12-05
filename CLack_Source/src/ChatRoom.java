ffa1930de5ba8cfbe96a8f76cf6df973a72af578
seanstev0710 <seanstev0710@gmail.com>
2023-12-04 21:27:14 -0800

    Typo on last ChatRoom Commit fixed

--- CLack_Source/src/ChatRoom.java
+++ CLack_Source/src/ChatRoom.java
@@ -3,7 +3,7 @@ import java.util.Vector;
 
 
 public class ChatRoom implements Serializable{
-	private static final long serialVersionUId = 10000000000l;
+	private static final long serialVersionUID = 1000000l;
 	private String chatID;
 	private ChatHistory history;
 	private Vector<Message> roomMessages;
